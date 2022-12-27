3389d9b44d4f3c64e7a5d53614704ea41f76d92b
scu008 <scu008@nate.com>
2022-12-28 02:13:51 +0900

    source update

--- float_comm/project-spec/meta-user/recipes-apps/udpapps/files/udpapps.c
+++ float_comm/project-spec/meta-user/recipes-apps/udpapps/files/udpapps.c
@@ -26,10 +26,23 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define reg_0 0x43c00000
 
 int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    int *reg = (int *)(reg_0);
+    int data, prv = 0;
 
+    // Repeatly print the register value
+    while(1){
+        data = *reg;
+        if(data != prv){
+            printf("%d\n", data);
+            prv = data;
+        }
+    }
+    
     return 0;
 }
