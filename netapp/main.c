74be63b5d7da97c3f3ecbf8ef34bec1adfd8d11a
scu008 <scu008@nate.com>
2022-12-28 21:31:54 +0900

    first success version

--- netapp/main.c
+++ netapp/main.c
@@ -4,24 +4,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
-
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
 
-// Memory map
+// Physical memory map
 #define reg_0 0x43c00000
 #define reg_offset 0x43c0ffff
 
+// UDP information
+#define PORT 5555
+#define BUFF_SIZE 100
 
 // Main function
 int main(int argc, char **argv)
 {
 
     // Main variables
-    int fd;
-    int *value;
+    int fd, i;
+    float *value;
+    int client_socket;
+    struct sockaddr_in serverAddress;
+    int server_addr_size;
+    float buff[BUFF_SIZE];
+
+
+    // UDP socket setup
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    inet_aton("192.168.0.2", (struct in_addr*) &serverAddress.sin_addr.s_addr);
+    serverAddress.sin_port = htons(PORT);
+
+    // Socket creation
+    if ((client_socket = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        printf("Creation is failed \n");
+        exit(0);
+    }
+    
+
 
     // call mmap function
     if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
@@ -30,18 +56,23 @@ int main(int argc, char **argv)
 	}
 
     // allocate physical memory to virtual memory;
-    value = (int *)mmap(NULL, reg_offset, (PROT_READ|PROT_WRITE), MAP_SHARED, m_fd, reg_0);
+    value = (float *)mmap(NULL, reg_offset, (PROT_READ|PROT_WRITE), MAP_SHARED, fd, reg_0);
     if( value == MAP_FAILED ) {
 		perror( "ERROR: mmap() failed, " );
 		close( fd );
 		exit(EXIT_FAILURE);
 	}
 
-    // Repeatly print the register value
+    // Repeatly send the register value
     while(1){
-        printf("%d", *value);
+        for(i = 0; i < BUFF_SIZE; i++){
+            buff[i] = *value;
+        }
+        sendto(client_socket, buff, sizeof(buff), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
     }
     
+
+
     // Exit instruction
     if( munmap( (void *)value, reg_offset ) != 0 )
 		perror("ERROR: munmap() failed, ");
--- netapp/petalinux@192.168.0.11
+++ netapp/petalinux@192.168.0.11
@@ -1,24 +1,50 @@
 
-
-
+// Header files
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
 
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// Memory map
 #define reg_0 0x43c00000
+#define reg_offset 0x43c0ffff
 
+
+// Main function
 int main(int argc, char **argv)
 {
-    int *reg = (int *)(reg_0);
-    int data, prv = 0;
+
+    // Main variables
+    int fd;
+    int *value;
+
+    // call mmap function
+    if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+		perror( "ERROR: could not open \"/dev/mem\", " );
+		exit(EXIT_FAILURE);
+	}
+
+    // allocate physical memory to virtual memory;
+    value = (int *)mmap(NULL, reg_offset, (PROT_READ|PROT_WRITE), MAP_SHARED, m_fd, reg_0);
+    if( value == MAP_FAILED ) {
+		perror( "ERROR: mmap() failed, " );
+		close( fd );
+		exit(EXIT_FAILURE);
+	}
 
     // Repeatly print the register value
     while(1){
-        data = *reg;
-        if(data != prv){
-            printf("%d\n", data);
-            prv = data;
-        }
+        printf("%d", *value);
     }
     
+    // Exit instruction
+    if( munmap( (void *)value, reg_offset ) != 0 )
+		perror("ERROR: munmap() failed, ");
+	close(fd);
     return 0;
 }
